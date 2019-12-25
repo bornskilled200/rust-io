07b9da8160f041b99c0c7493cae404fa8fb38be1
David Park <bornskilled200@gmail.com>
2019-12-25 09:54:10 +0700

    add basic functionality

--- src/main.rs
+++ src/main.rs
@@ -1,102 +1,83 @@
 #![feature(proc_macro_hygiene)]
 
 #[macro_use]
-extern crate diesel;
-#[macro_use]
 extern crate lazy_static;
 
 use serde::{Serialize, Deserialize};
 
 use thruster::{MiddlewareNext, MiddlewareReturnValue, MiddlewareResult};
-use thruster::{App, BasicContext as Ctx, Request, map_try};
+use thruster::{App, BasicContext as Context, Request, map_try};
 use thruster::thruster_middleware::send::file;
 use thruster::server::Server;
 use thruster::ThrusterServer;
 use thruster::thruster_proc::{async_middleware, middleware_fn};
-use diesel::{
-    prelude::*,
-    sqlite::SqliteConnection,
-    table,
-};
-use tokio_diesel::*;
-use std::time::{SystemTime, UNIX_EPOCH};
+use std::time::{SystemTime, UNIX_EPOCH, Duration};
 use thruster::errors::ThrusterError;
+use std::sync::{Mutex};
+use std::thread;
 
 #[derive(Serialize, Deserialize, Debug)]
-struct Data {
-    time: u128,
-    uptime: u128,
+struct Condition {
+    time: u64,
+    uptime: u64,
     air: f64,
 }
 
-#[derive(Debug)]
-enum MyError {
-    StdIoError(std::io::Error),
-}
-
-impl From<std::io::Error> for MyError {
-    fn from(e: std::io::Error) -> Self {
-        MyError::StdIoError(e)
-    }
-}
-
 #[middleware_fn]
-async fn index(context: Ctx, _next: MiddlewareNext<Ctx>) ->  MiddlewareResult<Ctx> {
+async fn index(context: Context, _next: MiddlewareNext<Context>) ->  MiddlewareResult<Context> {
     Ok(file(context, "public/index.html"))
 }
 
 #[middleware_fn]
-async fn stylesheet(context: Ctx, _next: MiddlewareNext<Ctx>) ->  MiddlewareResult<Ctx> {
+async fn stylesheet(context: Context, _next: MiddlewareNext<Context>) ->  MiddlewareResult<Context> {
     Ok(file(context, "public/stylesheets/style.css"))
 }
 
-#[derive(Queryable, Debug)]
-struct Condition {
-    id: i32,
-    time: i32,
-    uptime: i32,
-    air: f32,
-}
-
-table! {
-    conditions {
-        id -> Integer,
-        time -> Integer,
-        uptime -> Integer,
-        air -> Float,
-    }
+lazy_static! {
+    static ref DATA: Mutex<Vec<Condition>> = Mutex::new(Vec::new());
 }
 
 #[middleware_fn]
-async fn conditions_handler(mut context: Ctx, _next: MiddlewareNext<Ctx>) -> MiddlewareResult<Ctx> {
-    use crate::conditions::columns::{id, time, uptime, air};
-    context.status(404);
-    let connection = map_try!(SqliteConnection::establish("bme2.db"), Err(err) => {
-        ThrusterError { context, cause: Some(Box::new(err)), message: "connection".into(), status: 1 }
+async fn conditions_handler(mut context: Context, _next: MiddlewareNext<Context>) -> MiddlewareResult<Context> {
+    let data = map_try!(DATA.lock(), Err(err) => {
+            ThrusterError { context, cause: Some(Box::new(err)), message: "lock data".into(), status: 500 }
     });
-    let x = map_try!(conditions::table.load::<Condition>(&connection), Err(err) => {
-        ThrusterError { context, cause: Some(Box::new(err)), message: "connection".into(), status: 1 }
+    let result = map_try!(serde_json::to_string(&*data), Err(err) => {
+            ThrusterError { context, cause: Some(Box::new(err)), message: "serialize data".into(), status: 500 }
     });
-    context.body(&format!("{:?}", x));
-
+    context.body(&format!("{}", result));
 
     Ok(context)
 }
 
 #[middleware_fn]
-async fn four_oh_four(mut context: Ctx, _next: MiddlewareNext<Ctx>) -> MiddlewareResult<Ctx> {
+async fn four_oh_four(mut context: Context, _next: MiddlewareNext<Context>) -> MiddlewareResult<Context> {
     context.status(404);
     context.body("Whoops! That route doesn't exist!");
     Ok(context)
 }
 
 fn main() {
-    let app: App<Request, Ctx> = {
-        let mut app = App::<Request, Ctx>::new_basic();
-        app.set404(async_middleware!(Ctx, [four_oh_four]));
-        app.get("/", async_middleware!(Ctx, [index]));
-        app.get("/stylesheets/style.css", async_middleware!(Ctx, [stylesheet]));
-        app.get("/conditions", async_middleware!(Ctx, [conditions_handler]));
+    let start = SystemTime::now();
+
+    thread::spawn(move || {
+        for i in 1..20 {
+            let now = SystemTime::now();
+            DATA.lock().unwrap().push(Condition {
+                time: now.duration_since(UNIX_EPOCH).unwrap().as_secs(),
+                uptime: now.duration_since(start).unwrap().as_secs(),
+                air: i as f64,
+            });
+
+            thread::sleep(Duration::from_secs(i));
+        };
+    });
+    let app: App<Request, Context> = {
+        let mut app = App::<Request, Context>::new_basic();
+        app.set404(async_middleware!(Context, [four_oh_four]));
+        app.get("/", async_middleware!(Context, [index]));
+        app.get("/stylesheets/style.css", async_middleware!(Context, [stylesheet]));
+        app.get("/conditions", async_middleware!(Context, [conditions_handler]));
         app
     };
 
