c52a9c65c504e546d69df900e0496d61083e5f5e
David Park <bornskilled200@gmail.com>
2019-12-29 12:29:08 +0700

    more efficient database write

--- src/sensor.rs
+++ src/sensor.rs
@@ -1,11 +1,12 @@
 use serde::{Serialize, Deserialize};
-use std::fs::File;
+use std::fs::{File, OpenOptions};
 use std::error::Error;
-use std::io::{BufReader, Write};
+use std::io::{BufReader, Write, Read, Seek, SeekFrom};
 use err_ctx::ResultExt;
 use std::process::Command;
 use std::time::{SystemTime, UNIX_EPOCH};
 use std::sync::Mutex;
+use std::io::SeekFrom::End;
 
 lazy_static! {
     pub static ref DATA: Mutex<Vec<Condition>> = Mutex::new(Vec::new());
@@ -49,15 +50,28 @@ pub fn poll_condition() -> Result<Condition, Box<dyn Error>> {
 
 pub fn poll() -> Result<(), Box<dyn Error>> {
     let condition = poll_condition()?;
-    let json = DATA.lock().map(|mut vector| {
+    let mut json = serde_json::to_string(&condition)?;
+    DATA.lock().map(|mut vector| {
         if vector.len() > 3000 {
             vector.remove(0);
         }
         vector.push(condition);
-        serde_json::to_vec(&*vector).unwrap()
     })?;
-    let mut file = File::create("db.json")?;
-    Ok(file.write_all(&json)?)
+
+    let mut file = OpenOptions::new()
+        .write(true)
+        .create(true)
+        .open("db.json")?;
+    let original_size = file.metadata()?.len();
+    if original_size == 0 {
+        json.insert(0, '[');
+    } else {
+        file.seek(End(-1))?;
+        json.insert(0, ',');
+    }
+    json.push(']');
+    file.write_all(json.as_bytes())?;
+    Ok(())
 }
 
 pub fn get_conditions_json() -> Result<String, Box<dyn Error>> {
