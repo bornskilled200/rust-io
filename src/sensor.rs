a1eabf644625c7a0ca092928e9a00d97579dea92
David Park <bornskilled200@gmail.com>
2020-01-02 00:25:07 +0700

    better order of operations

--- src/sensor.rs
+++ src/sensor.rs
@@ -44,8 +44,8 @@ pub async fn load_database() -> Result<(), Box<dyn Error>>{
             } else {
                 0
             };
-            let mut conditions = CONDITIONS.lock().await;
             let drain = vector.drain(start..);
+            let mut conditions = CONDITIONS.lock().await;
             conditions.reserve(drain.len());
             Ok(conditions.extend(drain))
         }
