1c8cfa8c16ec2ddbb44d20bb1a3bb6289783dffc
David Park <bornskilled200@gmail.com>
2020-01-02 04:11:24 +0700

    better install script

--- install.sh
+++ install.sh
@@ -5,6 +5,7 @@ sudo mkdir -p /usr/local/bin
 sudo cp ./target/release/pms5003 /usr/local/bin/pms5003
 
 sudo mkdir -p /opt/pms5003
+sudo cp -r public/ /opt/pms5003/public/
 
 sudo cp ./pms5003.service /etc/systemd/system/pms5003.service
 sudo systemctl enable pms5003.service
